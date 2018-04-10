65ece0e32674f6b9bd27ff15b77a039031f1c75a
xwv648 <fei.yan@nokia-sbell.com>
2018-04-10 21:43:30 +0800

    fix scripts

--- countWordsAll.sh
+++ countWordsAll.sh
@@ -4,6 +4,7 @@ for fname in $(find _deploy/post -type f -a -name "*.html"); do
     count=$(cat "${fname}" | sed -n  "s/.*本文有\s*\([0-9]\+\).*/\1/gp")
     if [ -n "$count" ]; then
         total=$(($total+$count))
+        echo "$fname | $count"
     else
         echo "no count for ", $fname
     fi
